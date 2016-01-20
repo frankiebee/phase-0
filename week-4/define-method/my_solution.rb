dc376ae24538ca86d06f58c49147b6246c380ae0
frankiebee <frankie.diamond@gmail.com>
2016-01-19 17:10:17 -0800

    Create and Finish exercises

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,18 @@
+# Define an Empty Method
+def do_something(x,y,z)
+
+end
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def eat
+  puts "What do you like to eat"
+  x = gets.chomp
+  return x
+end
+
+y = eat
+
+puts "so i hear you like #{y}?"
