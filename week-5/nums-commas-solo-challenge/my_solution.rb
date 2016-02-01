82fc2e4971169ce250985eea3c7c946dd8b1de67
Frankie <frankie.diamond@gmail.com>
2016-01-31 16:29:11 -0800

    Add reflection

--- week-5/nums-commas-solo-challenge/my_solution.rb
+++ week-5/nums-commas-solo-challenge/my_solution.rb
@@ -20,16 +20,16 @@ for x in (0...in.length)
 
 # 1. Initial Solution
 def separate_comma(input)
-  input = input.to_s.reverse!
-  in_len = input.length
-  if in_len > 3
-    pad = in_len / 4
-    (3..in_len+pad).step(4) do |x|
-      input.insert(x,",")
-      input.chomp!(",")
+  input = input.to_s.reverse! # Make it a string and then reverse it to make it easier to insert commas
+  in_len = input.length #Get the length of the string 
+  if in_len > 3 # check if lentgh of string is grater then 3 
+    pad = in_len / 4 # during the insertion of commas length of string will increase. So pad is created to account for added charcters 
+    (3..in_len+pad).step(4) do |x| # enumerate the index at steps of 4 starting from 3
+      input.insert(x,",")#at index x inset a comma 
+      input.chomp!(",")#trim comma if 1 is at the -1 index
     end
   end
-  input.reverse
+  input.reverse#reverse the input back to orginal form and return it
 end
 
 
@@ -42,9 +42,13 @@ end
 
 # 3. Reflection
 =begin
-What was your process for breaking the problem down? What different approaches did you consider?
-Was your pseudocode effective in helping you build a successful initial solution?
-What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
-How did you initially iterate through the data structure?
-Do you feel your refactored solution is more readable than your initial solution? Why?
+My first atemtpt was all wrong as far as comma placement once i reversed it htough it was all good.
+this section took some trial and error:
+ (3..in_len+pad).step(4) do |x| # enumerate the index at steps of 4 starting from 3
+      input.insert(x,",")#at index x inset a comma 
+      input.chomp!(",")#trim comma if 1 is at the -1 index
+I learned a lot about ranges from this.
+My process was kind of flowy for this one. Had a super basic blueprint and let my brain do the rest. 
+Thats not typicaly how i would do it but i felt it was inportant this time around to really use the logics 
+in my head and develope some self relince in work flow.
 =end
