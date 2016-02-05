1c90242d7dc4dd88a50158c5d9e149910e75e5f1
frankiebee <frankie.diamond@gmail.com>
2016-02-05 13:57:51 -0800

    Add files and solutions

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -22,3 +22,38 @@ end
 
 
 # Reflection
+=begin
+##Release 1
+#What are these methods doing?
+Creating accessible and changeable instance variables.
+
+#How are they modifying or returning the value of instance variables?
+Reassigning returning the new variable using the declared methods.
+
+##Release 2
+What changed between the last release and this release?
+
+What was replaced?
+
+Is this code simpler than the last?
+
+##Release 3
+What changed between the last release and this release?
+
+What was replaced?
+
+Is this code simpler than the last?
+
+
+##Release 6
+What is a reader method?
+
+What is a writer method?
+
+What do the attr methods do for you?
+
+Should you always use an accessor to cover your bases? Why or why not?
+
+What is confusing to you about these methods?
+
+=end
