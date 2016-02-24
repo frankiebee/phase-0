f551ac10d7067103df492c732a3e65c858ed261c
Frankie Pangilinan <frankie.diamong@gmail.com>
2016-02-23 21:03:35 -0800

    refractor

--- week-8/ruby_review.rb
+++ week-8/ruby_review.rb
@@ -8,34 +8,8 @@
 # If you have difficulty, go into IRB and play with the methods.
 
 def dr_evils_cipher(coded_message)
-  input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the Ruby docs.
+  input = coded_message.downcase.split("")
   decoded_sentence = []
-  # cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
-  #           "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
-  #           "g" => "c",
-  #           "h" => "d",
-  #           "i" => "e",
-  #           "j" => "f",
-  #           "k" => "g",
-  #           "l" => "h",
-  #           "m" => "i",
-  #           "n" => "j",
-  #           "o" => "k",
-  #           "p" => "l",
-  #           "q" => "m",
-  #           "r" => "n",
-  #           "s" => "o",
-  #           "t" => "p",
-  #           "u" => "q",
-  #           "v" => "r",
-  #           "w" => "s",
-  #           "x" => "t",
-  #           "y" => "u",
-  #           "z" => "v",
-  #           "a" => "w",
-  #           "b" => "x",
-  #           "c" => "y",
-  #           "d" => "z"}
   cipher = Hash.new
   key = ('a'..'z').to_a
   key << key.shift(4) + ("0".."9").to_a
@@ -48,30 +22,7 @@ def dr_evils_cipher(coded_message)
       symbols.include?(input[check]) ? input[check] = " " : input[check] = cipher[input[check]]
       end
     end
-puts
-  # input.each do |x| # What is #each doing here?
-  #   found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
-  #   cipher.each_key do |y| # What is #each_key doing here?
-  #     if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
-  #       decoded_sentence << cipher[y]
-  #       found_match = true
-  #       break  # Why is it breaking here?
-  #     elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
-  #       decoded_sentence << " "
-  #       found_match = true
-  #       break
-  #     elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
-  #       decoded_sentence << x
-  #       found_match = true
-  #       break
-  #     end
-  #   end
-  #   if not found_match  # What is this looking for?
-  #     decoded_sentence << x
-  #   end
-  # end
   input.join("")
-  #What is this method returning?
 end
 
 # Your Refactored Solution
